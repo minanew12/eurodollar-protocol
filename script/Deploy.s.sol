fe24f038f92eed70bbb0299e5f4ea2a191e015b9
lollike <nikolajzl@hotmail.com>
2023-09-18 16:19:52 +0200

    Dev (#8)
    * fix tests
    
    * modify deploy script
    
    * add flip events

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -4,6 +4,7 @@
 pragma solidity ^0.8.19;
 
 import {Script} from "forge-std/Script.sol";
+import "forge-std/console.sol";
 import {EUD} from "../src/EUD.sol";
 import {EUI} from "../src/EUI.sol";
 import {YieldOracle} from "../src/YieldOracle.sol";
@@ -11,34 +12,32 @@ import {ERC1967Proxy} from "oz/proxy/ERC1967/ERC1967Proxy.sol";
 
 contract Deploy is Script {
 
-    bytes32 constant DEFAULT_ADMIN_ROLE = keccak256("DEFAULT_ADMIN_ROLE");
+    bytes32 constant DEFAULT_ADMIN_ROLE = 0x00;
 
     function run() external returns (address, address, address) {
-        address yieldOracle = _deployYieldOracle();
-        address eud = _deployEUD();
-        address eui = _deployEUI(eud, yieldOracle);
-        return (eud, eui, yieldOracle);
-    }
-
-    function _deployYieldOracle() internal returns (address) {
         vm.startBroadcast();
         YieldOracle oracle = new YieldOracle();
-        vm.stopBroadcast();
-        return address(oracle);
-    }
-
-    function _deployEUD() internal returns (address) {
-        vm.startBroadcast();
-        EUD eud = new EUD();
-        ERC1967Proxy eudProxy = new ERC1967Proxy(address(eud), abi.encodeWithSelector(EUD(address(0)).initialize.selector));
-        address(eudProxy).call(abi.encodeWithSignature("grantRole(bytes32,address)", DEFAULT_ADMIN_ROLE, address(this)));
+        oracle.adminUpdateCurrentPrice(1e18);
+        oracle.adminUpdateOldPrice(1e18);
+        console.log("Oracle address:");
+        console.log(address(oracle));
         
-        return address(eudProxy);
-    }
-    function _deployEUI(address eud, address yieldOracle) internal returns (address) {
-        vm.startBroadcast();
+        EUD eud = new EUD();
+        console.log("EUD address:");
+        console.log(address(eud));
+        ERC1967Proxy eudProxy = new ERC1967Proxy(address(eud), abi.encodeWithSelector(EUD(address(0)).initialize.selector));
+        //address(eudProxy).call(abi.encodeWithSignature("grantRole(bytes32,address)", DEFAULT_ADMIN_ROLE, admin));
+        console.log("EUDProxy address:");
+        console.log(address(eudProxy));
+        
         EUI eui = new EUI();
-        ERC1967Proxy euiproxy = new ERC1967Proxy(address(eui), abi.encodeWithSelector(EUI(address(0)).initialize.selector, eud, yieldOracle));
-        return address(euiproxy);
+        console.log("EUI address:");
+        console.log(address(eui));
+        ERC1967Proxy euiproxy = new ERC1967Proxy(address(eui), abi.encodeWithSelector(EUI(address(0)).initialize.selector, address(eud), address(oracle)));
+        console.log("EUIProxy address:");
+        console.log(address(euiproxy));
+        vm.stopBroadcast();
+
+        return (address(eud), address(eui), address(oracle));
     }
 }
